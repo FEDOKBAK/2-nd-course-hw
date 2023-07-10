8c5b87960767264f88cfbeb5cd4a878a6f571c78
fedokbak <fedokbak@gmail.com>
2023-07-10 07:36:13 +0300

    Completed hw-2

---
+++ hw-2.js
@@ -0,0 +1,63 @@
+/*let a = 10;
+alert (a);
+
+let a = 20;
+alert (a);*/
+
+
+/*let date = 2007;
+alert (date);*/
+
+
+/*let name = 'Брендан Эйх';
+alert (name);*/
+
+
+/*let x = 10;
+let y = 2;
+alert (x+y);
+alert (x*y);
+alert (x/y);*/
+
+
+/*let x = 2;
+let y = 5;
+let result = (x**y);
+alert (result);*/
+
+
+/*let a = 9;
+let b = 2;
+let result = (a/b);
+alert (result);*/
+
+
+/*let num = 1;
++= 5;
+-= 3;
+*= 7;
+/= 3;
+num++;
+num--;
+alert(num);*/
+
+
+/*let age = prompt("Сколько вам лет?");
+alert (age);*/
+
+
+/*const user = {
+  name: 'Fedor',
+  age: 23,
+  isAdmin: true,
+}
+user.cityOfresidence = 'Moscow';
+delete user.age;
+delete user.cityOfresidence;
+let info = prompt ("Какую информацию хотите узнать о пользователе?","name");
+console.log(user{info});
+alert (`Привет, ${user.name}!`)*/
+
+
+
+
