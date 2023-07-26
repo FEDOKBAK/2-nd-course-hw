453dd7e4968fc7cf128204231a1d2e8af17f8b8c
fedokbak <fedokbak@gmail.com>
2023-07-26 12:17:21 +0300

    hw-5 rework2

--- hw-5.js
+++ hw-5.js
@@ -123,22 +123,22 @@ console.log(circle2.gerPerimeter());*/
 
 //Задание 8
 
-/*function seasons() {
+function seasons() {
   let answer = prompt('Введите номер месяца от 1 до 12');
 
-  if (answer = 12 || (answer >= 1 && answer <=2)) {
+  if (answer == 12 || answer == 1 || answer == 2) {
     alert (`Зима`);
   } 
-  else if (answer >= 3 && answer <=5) {
+  else if (answer == 3 || answer == 4 || answer == 5) {
     alert (`Весна`);
   }
-  else if (answer >= 6 && answer <=8) {
+  else if (answer == 6 || answer == 7 || answer == 8) {
     alert (`Лето`);
   }
-  else if (answer >= 9 && answer <=11) {
+  else if (answer == 9 || answer == 10 || answer == 11) {
     alert (`Осень`);
   }
   else {
     alert (`Такого месяца нет`);
   }
-}*/
+}
